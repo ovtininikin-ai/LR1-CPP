77b8ce1db8924e0937e7053db773e45f5ed85d38
ovtininikin-ai <o.v.tininikin@student.khai.edu>
2026-09-11 11:50:22 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{// Завдання Begin29
+    // Розв'язання рівняння Ax + B = 0
+
+    double A, B, x;
+
+    cout << "Begin29" << endl;
+    cout << "Enter A (A != 0): ";
+    cin >> A;
+
+    cout << "Enter B: ";
+    cin >> B;
+
+    // Обчислення кореня рівняння
+    x = -B / A;
+
+    // Виведення результату
+    cout << "x = " << x << endl;
+
+    cout << endl;
+
+    // Завдання Begin32
+    // Знаходження діаметра і площі сектора
+
+    double R, D, S;
+    const double PI = 3.14;
+
+    cout << "Begin32" << endl;
+    cout << "Enter R: ";
+    cin >> R;
+
+    // Обчислення діаметра
+    D = 2 * R;
+
+    // Обчислення площі сектора 90 градусів
+    S = PI * R * R / 4;
+
+    // Виведення результатів
+    cout << "D = " << D << endl;
+    cout << "S = " << S << endl;
+
+    return 0;
+}
